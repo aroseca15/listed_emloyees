6e0eb8c760956eadc9973488c5852eafb350596d
aroseca15 <69179642+aroseca15@users.noreply.github.com>
2020-11-04 21:02:25 -0700

    Functionality of Add, Update working

--- dataEntry.js
+++ dataEntry.js
@@ -6,8 +6,8 @@ userPrompt();
 function userPrompt() {
     inquirer.prompt([
         {
-            message: "What would you like to see or make changes?",
             type: "rawlist",
+            message: "What would you like to see or make changes?",
             name: "staff",
             choices: ["Staff Members", "Positions", "Departments", "View Records"]
         },
@@ -34,16 +34,99 @@ function userPrompt() {
 
 
 function StaffSelect(res, err) {
-    console.log("direction works.");
+    inquirer.prompt([
+        {
+            type: "rawlist",
+            message: "What would you like to change?",
+            name: "employees",
+            choices: ["Add New Staff", "Make Updates", "Remove", "Back"]
+        },
+    ]).then((res) => {
+        switch (res.employees) {
+            case "Add New Staff":
+                AddNewEmployee()
+                break
+            case "Make Updates":
+                UpdateEmployee()
+                break
+            case "Remove":
+                DeleteEmployee()
+                break
+            case "Back":
+                userPrompt()
+                break
+        };
+    });
+
+    function AddNewEmployee() {
+        inquirer.prompt([
+            {
+                type: "input",
+                message: "What is your new hire's First Name?",
+                name: "firstName"
+            },
+
+            {
+                type: "input",
+                message: "What is your new hire's Last Name?",
+                name: "lastName"
+            },
+
+            {
+                type: "rawlist",
+                message: "What department were they assigned?",
+                name: "assignedD",
+                choices: ["Accounting", "Advertisement", "Client Services", "Creative Concepts", "HR", "Legal", "Software Testing"]
+            },
+
+            {
+                type: "input",
+                message: "What position will they be assigned?",
+                name: "jobTitle"
+            },
+        ]).then(function (res) {
+            return console.log("New Staff Member Saved.");
+            
+        });
+    };
+
+    // this will need to be connected to sql AND be changeable.
+    function UpdateEmployee(){
+        inquirer.prompt([
+            {
+                type: "input",
+                message: "Please enter the staff id or first and last name?",
+                name: "updateStaff"
+            },
+        ]).then(function (res){
+            return console.log(res.updateStaff +"'s profile has been updated")
+        })
+    }
     if (err) throw err;
 };
 
 function PositionSelect(res, err) {
+    inquirer.prompt([
+        {
+            type: "rawlist",
+            message: "What would you like to change?",
+            name: "position",
+            choices: ["Add New Position", "Make Updates", "Remove Position"]
+        },
+    ]).then();
     console.log("direction works.");
     if (err) throw err;
 };
 
 function DepartmentSelect(res, err) {
+    inquirer.prompt([
+        {
+            type: "rawlist",
+            message: "What would you like to change?",
+            name: "departments",
+            choices: ["Add New Department", "Make Updates", "Remove"]
+        },
+    ]).then();
     console.log("direction works.");
     if (err) throw err;
 };
@@ -66,4 +149,4 @@ function displayRecords() {
 
 
 
-module.exports="./dataEntry.js"
+module.exports = "./dataEntry.js"
--- seed.sql
+++ seed.sql
@@ -2,10 +2,10 @@ INSERT INTO staff_members(title, salary, department_id)
 VALUES (4, "Ray Benson", "Creative Concepts", "Manager", 75,000, 2);
 
 INSERT INTO staff_members(title, salary, department_id)
-VALUES (3, "Kelsea Ballerini", "Creative Concepts", "Team Lead", 55,000, 2);
+VALUES (3, "Kelsea Ballerini", "Creative Concepts", "Project Lead", 55,000, 2);
 
 INSERT INTO staff_members(title, salary, department_id)
-VALUES (2, "Colbie Caliet", "HR", "Team Lead", 55,000, 1);
+VALUES (2, "Colbie Caliet", "HR", "Project Lead", 55,000, 1);
 
 INSERT INTO staff_members (name,id)
 VALUES (1, "Jack Johnson", "HR", "Manager", 75,000, 1);
